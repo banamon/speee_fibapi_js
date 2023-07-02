e6838a2aea5ff4b3f542630bc7ca75f0ec455ddd
Taga <tagahei0505@gmail.com>
2023-07-03 03:46:50 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// expressモジュールを読み込む
+const express = require('express');
+
+// expressアプリを生成する
+const app = express();
+
+// http://localhost:3000/api/v1/list にアクセスしてきたときに
+// TODOリストを返す
+app.get('/', (req, res) => {
+  const n = req.query.n;
+  console.log(req);
+  const response = {
+    message: `hello` + n
+  };
+    res.json(response);
+});
+
+// ポート3000でサーバを立てる
+app.listen(3000, () => console.log('Listening on port 3000'));
